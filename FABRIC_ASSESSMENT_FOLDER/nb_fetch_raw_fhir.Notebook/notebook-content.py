bbc87c1d5f1524d8d04b5363bca3f3f36d7142b4
manoj stars <88538912+Manojstars@users.noreply.github.com>
2026-03-23 21:24:41 +0530

    Committing 2 items from workspace 70296d80-3b83-4453-828e-8921e196c568

---
+++ FABRIC_ASSESSMENT_FOLDER/nb_fetch_raw_fhir.Notebook/notebook-content.py
@@ -0,0 +1,34 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "488995c7-c22b-4ea2-8875-1e538f017033",
+# META       "default_lakehouse_name": "FHIR_Lakehouse",
+# META       "default_lakehouse_workspace_id": "70296d80-3b83-4453-828e-8921e196c568",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "488995c7-c22b-4ea2-8875-1e538f017033"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
